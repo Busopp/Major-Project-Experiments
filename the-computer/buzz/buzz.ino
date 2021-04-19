9c47aaaf8637c20c3ddf939aa287d77957a89dbf
Bosco <bosco.bartilomo@gmail.com>
2021-04-19 10:22:03 +1000

    Added arduino scripts for this computer

---
+++ the-computer/buzz/buzz.ino
@@ -0,0 +1,24 @@
+// change pin that buzzer is connected to here
+#define buzzer  3
+
+void setup() {
+  pinMode(buzzer, OUTPUT);
+}
+
+void loop() {
+  unsigned char i, j;
+  // pulse the buzzer on for a short time
+//  for (i = 0 ; i < 80; i++) { // Wen a frequency sound
+//    digitalWrite(buzzer, HIGH); // send voice
+//    delay(1); // Delay 1ms
+//    digitalWrite(buzzer, LOW); // do not send voice
+//    delay(1); // delay ms
+//    }
+    
+//   for(i = 0; i < 100; i++) { // Wen Qie out another frequency sound
+//    digitalWrite(buzzer, HIGH); // send voice
+//    delay(2); // delay 2ms
+//    digitalWrite(buzzer, LOW); // do not send voice.
+//    delay(2); // delay 2ms
+//   }
+}
