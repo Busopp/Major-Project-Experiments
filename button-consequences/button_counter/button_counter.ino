80713a5ec66ea0c3313e281e4011ca5bcf7ae17b
Bosco <bosco.bartilomo@gmail.com>
2021-05-07 12:30:47 +1000

    make buttons great again

--- button-consequences/button_counter/button_counter.ino
+++ button-consequences/button_counter/button_counter.ino
@@ -3,7 +3,7 @@
 // blink a led according to count
 
 byte switchPinG = 8;                    // switch is connected to pin 2
-byte switchPinB = 8; 
+byte switchPinB = 9; 
 byte ledPin = 13;                      // led on pin 13
 byte buttonPressesG = 0;                // how many times the button has been pressed 
 byte lastPressCountG = 0;               // to keep track of last press count
@@ -16,6 +16,8 @@ void setup() {
   digitalWrite(switchPinG, HIGH);      // set pullup resistor
   pinMode(switchPinB, INPUT);          // Set the switch pin as input
   digitalWrite(switchPinB, HIGH);      // set pullup resistor
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
   Serial.begin(9600);                 // Set up serial communication at 9600bps
 }
 
@@ -23,25 +25,30 @@ void loop(){
   if (digitalRead(switchPinG) == LOW)  // check if button was pressed
   {
     buttonPressesG++;                  // increment buttonPresses count
+    digitalWrite(ledPin, LOW);
     delay(450);                       // debounce switch
+    digitalWrite(ledPin, HIGH);
   } 
   if (digitalRead(switchPinB) == LOW)  // check if button was pressed
   {
     buttonPressesB++;                  // increment buttonPresses count
+    digitalWrite(ledPin, LOW);
     delay(450);                       // debounce switch
+    digitalWrite(ledPin, HIGH);
   }
   
   if (lastPressCountG != buttonPressesG)              // only do output if the count has changed
   {
-    Serial.print ("Button press count = ");          // out to serial
+    Serial.print ("Green button press count = ");          // out to serial
     Serial.println(buttonPressesG, DEC);
 
     lastPressCountG = buttonPressesG;    // track last press count
   }
   if (lastPressCountB != buttonPressesB)              // only do output if the count has changed
   {
-    Serial.print ("Button press count = ");          // out to serial
+    Serial.print ("Blue button press count = ");          // out to serial
     Serial.println(buttonPressesB, DEC);
 
     lastPressCountB = buttonPressesB;    // track last press count
   }
+}
